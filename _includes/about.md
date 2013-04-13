0613c5821e939c6fccaf6287720d5f24b6d72165
neilang <neilang@gmail.com>
2013-04-13 16:06:11 +1000

    Converted to Jekyll project

---
+++ _includes/about.md
@@ -0,0 +1,3 @@
+Markdown was created by [John Gruber](http://daringfireball.net/projects/markdown/). For a list of all syntax options see the [Markdown project page](http://daringfireball.net/projects/markdown/syntax).
+
+This site was created by [Neil Ang](http://neilang.com/) and [Jason Smith](http://waitingallday.com/), and released under Creative Commons.
---
+++ resources/javascripts/site.js
@@ -0,0 +1,39 @@
+$(function() {
+	// Print this page
+	if ($('#print').length>0) {
+		$('#print').click(function(e){
+			e.preventDefault();
+			window.print();
+		});
+	}
+
+    // Tabby
+    $('ul.nav').children('li').on('mouseover click tap', function(){
+        $(this).closest('ul.nav').children('li').removeClass('on');
+        $(this).addClass('on');
+        $(this).closest('div').children('blockquote').removeClass('on');
+        var curr = $(this).data('rel');
+        $(this).closest('div').children('blockquote').each(function(){
+            if ($(this).data('title')==curr) $(this).addClass('on');
+        });
+    });
+
+    $('.marker').each(function(i) {
+        var position = $(this).position();
+        $(this).scrollspy({
+            min: position.top,
+            max: position.top + $(this).height(),
+            onEnter: function(element, position) {
+                $("#top a").removeClass("on");
+                $("#n-"+$(element).attr("id")).addClass("on");
+            },
+            onLeave: function(element, position) {
+//                $("#top a").removeClass("on");
+            }
+        });
+    });
+    $('#top a').on('click tap', function() {
+        $("#top a").removeClass("on");
+        $(this).addClass("on");
+    });
+});
