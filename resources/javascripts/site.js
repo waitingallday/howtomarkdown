3d4c688650574045b12eea3c3e0581eae17cea48
Jason Smith <jason@waitingallday.com>
2014-11-14 15:17:54 +1100

    clean up styles a little, update placeholder image

--- resources/javascripts/site.js
+++ resources/javascripts/site.js
@@ -6,24 +6,13 @@ $(function() {
             min: position.top,
             max: position.top + $(this).height(),
             onEnter: function(element, position) {
-                $("#top a").removeClass("on");
+                $("header a").removeClass("on");
                 $("#n-"+$(element).attr("id")).addClass("on");
             }
         });
     });
-    $('#top a').on('click tap', function() {
-        $("#top a").removeClass("on");
+    $('header a').on('click tap', function() {
+        $("header a").removeClass("on");
         $(this).addClass("on");
-    });
-
-    // Tabby
-    $('ul.nav').children('li').on('mouseover click tap', function(){
-        $(this).closest('ul.nav').children('li').removeClass('on');
-        $(this).addClass('on');
-        $(this).closest('div').children('blockquote').removeClass('on');
-        var curr = $(this).data('rel');
-        $(this).closest('div').children('blockquote').each(function(){
-            if ($(this).data('title')==curr) $(this).addClass('on');
-        });
     });
 });
